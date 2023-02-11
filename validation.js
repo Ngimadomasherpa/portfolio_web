060dfa31f3cab2041a83770fc73cd41c00e141db
Ngima Doma Sherpa <np03cs4a220322@heraldcollege.edu.np>
2023-02-11 19:56:26 +0545

    My first portfolio website

---
+++ validation.js
@@ -0,0 +1,19 @@
+function validate(){
+var username = document.getElementById("uname").value;
+var password = document.getElementById("pass").value;
+
+if (username.trim()==""){
+    alert("Username must be filled.");
+    return false;
+}else if(password.trim()==""){
+    alert("Password must be filled");
+    return false;
+}
+else if (password.length<5){
+    alert("Password too short.")
+    return false;
+}
+else{
+    alert("Logged in successfully");
+}
+}
